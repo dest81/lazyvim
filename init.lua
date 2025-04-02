2bc44647b6800aa49b5e2d8bf9335e17e9eb48dc
Ihor Marhitych <imarhitych@ripe.net>
2025-04-02 20:04:32 +0200

    catppuccin theme

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- require("monokai-pro")._load("octagon")
+-- require("catppuccin").load("mocha")
+require("catppuccin").load("frappe")
