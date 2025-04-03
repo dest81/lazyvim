2654acf0642e8b817bf1db524cebc55bc74d99f8
Ihor Marhitych <imarhitych@ripe.net>
2025-04-03 22:11:27 +0200

    monokai

---
+++ lua/plugins/monokai.lua
@@ -0,0 +1,8 @@
+return {}
+-- return {
+--   "loctvl842/monokai-pro.nvim",
+--   config = function()
+--     -- Set the colorscheme to Monokai
+--     -- vim.cmd("colorscheme monokai")
+--   end,
+-- }
