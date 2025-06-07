b4ce2512aaf9897a5748ba8a2524bece88a83273
Ihor Marhitych <imarhitych@ripe.net>
2025-06-07 19:13:57 +0200

    super-tab bad idea for autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -18,12 +18,12 @@ return {
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = "super-tab" },
+    -- keymap = { preset = "super-tab" },
 
     completion = {
       list = {
         selection = {
-          preselect = false,
+          -- preselect = false,
           -- auto_insert = false,
         },
       },
