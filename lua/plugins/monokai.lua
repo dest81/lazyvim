c3cb18fdc6f86b3fb64656615be33106869c5e40
Ihor Marhitych <imarhitych@ripe.net>
2025-07-26 17:43:28 +0200

    add Monokai theme

--- lua/plugins/monokai.lua
+++ lua/plugins/monokai.lua
@@ -1,4 +1,13 @@
-return {}
+return {
+  {
+    "tanvirtin/monokai.nvim",
+    lazy = false, -- load on startup
+    priority = 1000, -- make sure it loads before others
+    config = function()
+      vim.cmd([[colorscheme monokai]])
+    end,
+  },
+}
 -- return {
 --   "loctvl842/monokai-pro.nvim",
 --   config = function()
