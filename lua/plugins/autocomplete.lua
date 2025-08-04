49f8f1bad895aa5dc9db7c5926d43b962db8da50
Ihor Marhitych <imarhitych@ripe.net>
2025-08-04 22:36:18 +0200

    configure autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -21,6 +21,15 @@ return {
     -- 'super-tab' stopped working for some reason, let's change default to work with tab
     keymap = { preset = "default", ["<Tab>"] = { "accept", "fallback" } },
     completion = {
+      menu = {
+        border = "rounded",
+        winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+      },
+      documentation = {
+        window = {
+          border = "rounded",
+        },
+      },
       list = {
         selection = {
           preselect = true,
@@ -29,15 +38,15 @@ return {
       },
     },
 
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = "mono",
-    },
+    -- appearance = {
+    --   -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+    --   -- Useful for when your theme doesn't support blink.cmp
+    --   -- Will be removed in a future release
+    --   use_nvim_cmp_as_default = true,
+    --   -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    --   -- Adjusts spacing to ensure icons are aligned
+    --   nerd_font_variant = "mono",
+    -- },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
