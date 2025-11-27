ebaea6c35bd1e7e7793eef6c7e67e654efe88f5e
Ihor Marhitych <imarhitych@ripe.net>
2025-11-27 22:04:44 +0100

    disable sunglasses - buggy

--- lua/plugins/sunglasses.lua
+++ lua/plugins/sunglasses.lua
@@ -1,25 +1,26 @@
-return {
-  "miversen33/sunglasses.nvim",
-  config = true,
-  opts = {
-    filter_type = "NOSYNTAX",
-    filter_percent = 0.45,
-    refresh_timer = 5, -- Auto refresh interval in seconds
-    excluded_filetypes = {
-      "neo-tree",
-      "dashboard",
-      "lazy",
-      "TelescopePrompt",
-      "neo-tree",
-      "NvimTree",
-      "alpha",
-      "toggleterm",
-      "notify",
-      "noice", -- etc.
-    },
-    excluded_highlights = {
-      "WinSeparator",
-      { "lualine_.*", glob = true },
-    },
-  },
-}
+return {}
+-- return {
+--   "miversen33/sunglasses.nvim",
+--   config = true,
+--   opts = {
+--     filter_type = "NOSYNTAX",
+--     filter_percent = 0.45,
+--     refresh_timer = 5, -- Auto refresh interval in seconds
+--     excluded_filetypes = {
+--       "neo-tree",
+--       "dashboard",
+--       "lazy",
+--       "TelescopePrompt",
+--       "neo-tree",
+--       "NvimTree",
+--       "alpha",
+--       "toggleterm",
+--       "notify",
+--       "noice", -- etc.
+--     },
+--     excluded_highlights = {
+--       "WinSeparator",
+--       { "lualine_.*", glob = true },
+--     },
+--   },
+-- }
