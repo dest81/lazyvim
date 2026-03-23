f552c35e30cbe60c8db3f6741412ad12909386bb
Ihor Marhitych <imarhitych@ripe.net>
2026-03-23 20:53:32 +0100

    custom neotest-python

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,17 +1,29 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
-    "nvim-neotest/neotest-python",
+    "dest81/neotest-python",
   },
   opts = {
     adapters = {
       ["neotest-python"] = {
-        -- python = ".venv/bin/python", -- or "python3"
+        -- python = "venv/bin/python", -- or "python3"
 
         args = {
           "--reuse-db",
           "--disable-warnings",
+          -- "-c",
+          -- "pyproject.toml",
         },
+        pytest_discovery = true,
+        test_function_patterns = "^(test|it|ensure|must|should)",
+        runner = "pytest", -- Ensure pytest is explicitly set
+        -- pytest_discover_instances = true,
+        is_test_file = function(file_path)
+          return file_path:match("_spec%.py$")
+            or file_path:match("_specs%.py$")
+            or file_path:match("_example%.py$")
+            or file_path:match("_examples%.py$")
+        end,
       },
     },
   },
